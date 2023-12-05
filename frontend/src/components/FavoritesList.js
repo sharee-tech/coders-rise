d1a39d564f2ec8797b7116fa7422413e40c196a7
sharee-tech <mail.sharee@gmail.com>
2023-12-04 18:19:52 -0600

    working on button to add colleges to a comparison array

--- frontend/src/components/FavoritesList.js
+++ frontend/src/components/FavoritesList.js
@@ -5,20 +5,42 @@ import Button from "./Button";
 export default function FavoritesList() {
   const [selectedFavorite, setSelectedFavorite] = useState(null);
   const [colleges, setColleges] = useState(data);
+  const [compareCollege, setCompareCollege] = useState([]);
+
   return (
     <ul className="list-group">
       {colleges.map((college) => (
-        <Favorite key={college["school.name"]} college={college} />
+        <Favorite
+          key={college["school.name"]}
+          college={college}
+          compare={compareCollege}
+          onCompare={setCompareCollege}
+        />
       ))}
     </ul>
   );
 }
 
-function Favorite({ college }) {
+function Favorite({ college, compare, onCompare }) {
+  function handleConfirm(college) {
+    // check to see if college is in the "compare" array.
+    // if it is, remove it, if it is not, add it.
+    if (compare.includes(college.id)) {
+      console.log(compare);
+      onCompare(compare.filter((a) => a.id === college.id));
+    } else {
+      console.log(compare);
+      onCompare((compare) => [...compare, college.id]);
+    }
+  }
+
   return (
     <li className="list-group-item">
-      <h3>{college["school.name"]}</h3>
-      <Button onClick={() => alert("compare")}>Compare</Button>
+      <h3 className={compare.includes(college.id) ? "mark" : ""}>
+        {college["school.name"]}
+      </h3>
+      <Button onClick={() => handleConfirm(college)}>Compare</Button>
+      <p>{compare}</p>
     </li>
   );
 }
