06e71ddd9013c164e3252409e946cdc8283f7e8b
graycummins <>
2023-12-17 15:31:12 -0600

    updated restructure

---
+++ src/main/java/org/example/models/User.java
@@ -0,0 +1,51 @@
+package org.example.models;
+
+public class User {
+    private int userID;
+    private String username;
+    private String password;
+    private String email;
+
+    // Constructors, getters, setters
+
+    public User(int userID, String username, String password, String email) {
+        this.userID = userID;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    // Getters and setters for the fields
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/org/example/services/CollegeService.java
@@ -0,0 +1,38 @@
+package org.example.services;
+
+
+import org.example.models.College;
+import org.example.repositories.CollegeRepository;
+
+import java.util.List;
+
+public class CollegeService {
+    private CollegeRepository collegeRepository;
+
+    public CollegeService(CollegeRepository collegeRepository) {
+        this.collegeRepository = collegeRepository;
+    }
+
+    // Add a college to the favorites list for a user
+    public void addCollegeToFavorite(String username, String collegeID, String customNote) {
+        collegeRepository.addCollegeToFavorite(username, collegeID, customNote);
+    }
+
+    // Pull up news articles for a college
+    public List<String> getCollegeNews(String collegeID) {
+        return collegeRepository.getCollegeNews(collegeID);
+    }
+
+    // Fetch colleges based on filters
+    public List<College> filterCollegesByCriteria(String location, String degreeType, String major,
+                                                  int studentBodySize, double tuition) {
+        return collegeRepository.filterColleges(location, degreeType, major, studentBodySize, tuition);
+    }
+
+    // Compare colleges from saved favorites
+    public List<College> compareFavoriteColleges(String username) {
+        return collegeRepository.compareFavoriteColleges(username);
+    }
+
+    // Other college-related functionalities can be added here
+}
