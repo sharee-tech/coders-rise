e378326f3a727f60c925b526b3de9956e1c5f186
sharee-tech <84551254+sharee-tech@users.noreply.github.com>
2023-12-19 21:24:53 -0600

    Delete duplicate src/main directory

--- src/main/java/org/example/configurations/DatabaseConfig.java
+++
@@ -1,8 +0,0 @@
-/*
-Configures and manages database-related settings and connections, defining
-database-specific configurations for the application.
- */
-package org.example.configurations;
-
-public class DatabaseConfig {
-}
