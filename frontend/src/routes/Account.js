07ddfa97d9e84c7ffbd95e78836efa5596b5cc6a
Sharion2023 <Sharion@gmail.com>
2023-12-21 14:49:54 -0600

    Full stack set up.

--- frontend/src/routes/Account.js
+++ frontend/src/routes/Account.js
@@ -1,5 +1,5 @@
 import Navbar from "../components/Navbar";
-import Profile from "../../src/components/Profile";
+import Profile from "../components/Profile";
 import AddCollege from "../components/AddCollege";
 
 export default function Account() {
